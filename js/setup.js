af7209aaaeca96835cc13d461d9c8d146f7b67c1
Sergey <erofeev28@gmail.com>
2019-09-26 17:58:42 +0600

    Выполнил 4 ДЗ

--- js/setup.js
+++ js/setup.js
@@ -40,6 +40,15 @@ var eyesColors = [
   'green'
 ];
 
+var fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+
 // Перестановка местами имени и фамилии
 var nameOrSurname = function () {
   var coin = window.randomVal(0, 1);
@@ -69,6 +78,7 @@ var generationData = function (count) {
   return persons;
 };
 
+
 // Рисуем DOM
 var renderWizard = function (person) {
   var similarTemplate = document.querySelector('#similar-wizard-template').content;
@@ -87,6 +97,7 @@ var renderWizard = function (person) {
   return wizard;
 };
 
+
 // Добавляем персонажа
 var addWizard = function (persons) {
   for (var i = 0; i < persons.length; i++) {
@@ -95,6 +106,7 @@ var addWizard = function (persons) {
   }
 };
 
+
 // Окно персонажей
 var setup = document.querySelector('.setup');
 // Блок персонажей
@@ -102,9 +114,130 @@ var setupSimilar = setup.querySelector('.setup-similar');
 // Список персонажей
 var similarList = setupSimilar.querySelector('.setup-similar-list');
 
-setup.classList.remove('hidden');
 setupSimilar.classList.remove('hidden');
 
 var persons = generationData(4);
 
 addWizard(persons);
+
+// ------------------------------------
+var setupOpen = document.querySelector('.setup-open');
+var iconSetupOpen = setupOpen.querySelector('.setup-open-icon');
+var setupClose = document.querySelector('.setup-close');
+var setupUserName = setup.querySelector('.setup-user-name');
+
+var setupWizard = setup.querySelector('.setup-wizard');
+var setupWizardCoat = setupWizard.querySelector('.wizard-coat');
+var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
+var setupFireball = setup.querySelector('.setup-fireball-wrap');
+
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
+// Открытие попап окна с настройками персонажа
+var openSetup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onSetupEscPress);
+};
+
+// Закрытие попап окна с настройками персонажа
+var closeSetup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onSetupEscPress);
+};
+
+// Открытие попап окна с настройками персонажа при нажатии на ENTER
+var onSetupOpenEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openSetup();
+  }
+};
+
+// Закрытие попап окна с настройками персонажа при нажатии на ENTER
+var onSetupCloseEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closeSetup();
+  }
+};
+
+// Закрытие попап окна с настройками персонажа при нажатии на ESC
+var onSetupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeSetup();
+  }
+};
+
+// Событие клика на иконку пользователя
+setupOpen.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  openSetup();
+});
+
+// Событие клика на крестик
+setupClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+
+  closeSetup();
+});
+
+// При фокусе на иконке пользователя добавляем обработчик события ENTER
+iconSetupOpen.addEventListener('focus', function () {
+  document.addEventListener('keydown', onSetupOpenEnterPress);
+});
+// При отмене фокуса на иконке пользователя убираем обработчик события ENTER
+iconSetupOpen.addEventListener('blur', function () {
+  document.removeEventListener('keydown', onSetupOpenEnterPress);
+});
+
+// При фокусе на поле ввода имени добавляем обработчик события ESC
+setupUserName.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onSetupEscPress);
+});
+// При отмене фокуса на поле ввода имени убираем обработчик события ESC
+setupUserName.addEventListener('blur', function () {
+  document.addEventListener('keydown', onSetupEscPress);
+});
+
+// При фокусе на крестике добавляем обработчик события ENTER
+setupClose.addEventListener('focus', function () {
+  document.addEventListener('keydown', onSetupCloseEnterPress);
+});
+// При отмене фокуса на крестике добавляем убираем обработчик события ENTER
+setupUserName.addEventListener('blur', function () {
+  document.removeEventListener('keydown', onSetupCloseEnterPress);
+});
+
+// Проверка на валидность данных
+setupUserName.addEventListener('invalid', function () {
+  if (setupUserName.validity.tooShort) {
+    setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (setupUserName.validity.tooLong) {
+    setupUserName.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (setupUserName.validity.valueMissing) {
+    setupUserName.setCustomValidity('Обязательное поле');
+  } else {
+    setupUserName.setCustomValidity('');
+  }
+});
+
+// Цвет одежды
+setupWizardCoat.addEventListener('click', function (evt) {
+  var target = evt.target;
+  target.setAttribute('style', 'fill: ' + coatColors[window.randomVal(0, coatColors.length - 1)]);
+});
+
+// Цвет глаз
+setupWizardEyes.addEventListener('click', function (evt) {
+  var target = evt.target;
+  target.setAttribute('style', 'fill: ' + eyesColors[window.randomVal(0, eyesColors.length - 1)]);
+});
+
+// Цвет фаирбола
+setupFireball.addEventListener('click', function () {
+  var input = setupFireball.querySelector('[name="fireball-color"]');
+  var color = fireballColors[window.randomVal(0, fireballColors.length - 1)];
+
+  setupFireball.setAttribute('style', 'background: ' + color);
+  input.value = color;
+});
